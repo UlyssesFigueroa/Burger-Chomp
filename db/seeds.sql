cfc0f131a00ae6c45d018405e58ce66cba82404d
Ulysses <This2willpass@aol.com>
2019-11-23 00:07:05 -0700

    updates to connection.js for Heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+USE burger_db;
+
 INSERT INTO burgers (name) VALUES ('Cheeseburger');
 INSERT INTO burgers (name) VALUES ('Bacon Burger');
 INSERT INTO burgers (name, devoured) VALUES ('Double Cheeseburger', true);
