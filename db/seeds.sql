06a8b11bc08dfa58ebc0c035a1213d85bbce3b0b
Ulysses <This2willpass@aol.com>
2019-11-22 23:30:41 -0700

    Directory Created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Impossible Burger', true);
